b28836225b9b8a13cf6de23ae9b8b53a83f4d5e6
ナカジマトオル <toru98til@gmail.com>
2022-12-02 17:54:24 +0900

    headerとhamburger内で起こるバグを解消

--- sources/script/script.js
+++ sources/script/script.js
@@ -1,6 +1,14 @@
+//////////
+// test //
+//////////
+
 console.log("Hello, World");
 console.log("Hello, Japan");
 console.log("Hello, EN");
+
+///////////////
+// hamburger //
+///////////////
 
 window.addEventListener("load", function () {
   const ham = document.getElementById("js-ham");
@@ -11,11 +19,37 @@ window.addEventListener("load", function () {
 
   ham.addEventListener("click", function () {
     console.log("OK");
+    console.log(window.pageYOffset);
+    console.log(window.outerHeight);
     nav.classList.toggle("js-nav__in");
   });
 });
 
-// hellow worldß
+////////////
+// header //
+////////////
+const hed = document.getElementById("js-header");
+window.addEventListener("scroll", function () {
+  // メディアクエリ
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    console.log("mq!");
+    // urlの取得
+    let url = location.href;
+    if (url == "http://localhost:8080/index.html") {
+      if (window.pageYOffset > window.outerHeight - 100) {
+        console.log("ヨイショ");
+        hed.classList.add("js-fadein");
+      }
+    } else {
+      hed.classList.add("js-fadein");
+    }
+  } else {
+    console.log("mq?");
+    return false;
+  }
+});
+
+// hellow world
 
 import p5 from "p5";
 import { sketch } from "./sketch.js";
