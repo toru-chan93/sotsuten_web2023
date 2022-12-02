573b5ca90247b2612f220e38c51ae425966ceea5
ナカジマトオル <toru98til@gmail.com>
2022-12-02 11:31:26 +0900

    navigationのjsを実装

--- sources/script/script.js
+++ sources/script/script.js
@@ -1,6 +1,21 @@
 console.log("Hello, World");
 console.log("Hello, Japan");
-// hellow world
+console.log("Hello, EN");
+
+window.addEventListener("load", function () {
+  const ham = document.getElementById("js-ham");
+  const nav = document.getElementById("js-nav");
+  if (!ham) {
+    console.log("!ham");
+  }
+
+  ham.addEventListener("click", function () {
+    console.log("OK");
+    nav.classList.toggle("js-nav__in");
+  });
+});
+
+// hellow worldß
 
 import p5 from "p5";
 import { sketch } from "./sketch.js";
