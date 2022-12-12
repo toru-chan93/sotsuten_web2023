0f33f485c2c33210a830c2b26f130c611063100e
neko_kurage <hosiman1923@gmail.com>
2022-12-12 16:34:04 +0900

    スクロールとハートのモーション用のアニメーションを適用

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-module.exports = {
+const script = {
   mode: "development",
   entry: "./sources/script/script.js",
   output: {
@@ -8,3 +8,25 @@ module.exports = {
     path: path.resolve(__dirname, "./public/assets/script"),
   },
 };
+
+const scroll = {
+  mode: "development",
+  entry: "./sources/script/scroll.js",
+  output: {
+    filename: "scroll.js",
+    path: path.resolve(__dirname, "./public/assets/script"),
+  },
+};
+
+const three_script= {
+  mode: "development",
+  entry: "./sources/script/three_script.js",
+  output: {
+    filename: "three_script.js",
+    path: path.resolve(__dirname, "./public/assets/script"),
+  },
+};
+
+module.exports = [
+  script, scroll, three_script
+];
